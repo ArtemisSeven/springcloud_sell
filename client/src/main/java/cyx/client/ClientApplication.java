b7e59010e235dd713709c5f97c8be67576db38d8
yourname <youremail>
2019-03-13 11:23:16 +0800

    开发client端

---
+++ client/src/main/java/cyx/client/ClientApplication.java
@@ -0,0 +1,15 @@
+package cyx.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient//声明为是client端
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+}
